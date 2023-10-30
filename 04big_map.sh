231f5cf9045ddff25107cdd3af4ac4da9d213c63
HaoyuLang <149399074+HaoyuLang@users.noreply.github.com>
2023-10-30 23:17:25 +0800

    Update 04big_map.sh

--- 04big_map.sh
+++ 04big_map.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# This script runs the BiG-MAP tool to analyze the abundance of secondary metabolite gene clusters in the gut microbiomes of honey bees.
-# BiG-MAP is used to estimate the abundance of Biosynthetic Gene Clusters (BGCs) using metagenomic data.
-
-# Usage: ./run_bigmap.sh <antismash_directory> <bigscape_directory> <pfam_directory> <output_directory>
+# This script runs BiG-MAP to analyze the abundance of secondary metabolite gene clusters in honey bee gut microbiomes.
+# BiG-MAP is a tool for estimating the abundance of Biosynthetic Gene Clusters (BGCs) using metagenomic data.
 
 # Check if all required arguments are provided
-if [[ $# -ne 4 ]]; then
-    echo "Usage: $0 <antismash_directory> <bigscape_directory> <pfam_directory> <output_directory>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <big_map_script> <metadata_file> <input_files_1> <input_files_2> <output_directory> <family_directory>"
     exit 1
 fi
 
-# Parse command line arguments
-antismash_dir="$1"
-bigscape_dir="$2"
-pfam_dir="$3"
-output_dir="$4"
+# Assign input arguments to variables
+big_map_script="$1"     # Path to BiG-MAP's BiG-MAP.map.py script
+metadata_file="$2"      # Path to the metadata file
+input_files_1="$3"      # Path to input files 1
+input_files_2="$4"      # Path to input files 2
+output_directory="$5"   # Path to the output directory
+family_directory="$6"   # Path to the family directory
 
-# Command to run BiG-MAP for abundance estimation.
-python3 /home/langhaoyu/BIG-MAP/src/BiG-MAP.family.py \
-    -D "$antismash_dir" \
-    -b "$bigscape_dir" \
-    -pf "$pfam_dir" \
-    -O "$output_dir"
+# Command to run BiG-MAP
+python3 "$big_map_script" -b "$metadata_file" -I1 "$input_files_1" -I2 "$input_files_2" -O "$output_directory" -F "$family_directory"
